73c6b487086f55432c62c54baf6b7312916b92ea
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-01 17:57:58 +0200

    add a login form in form.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms import StringField,TextAreaField,SubmitField,ValidationError,SelectField
 from wtforms.validators import Required,Email
 from ..models import User
 
-class ReviewForm(FlaskForm):
+class CommentForm(FlaskForm):
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+    comment = TextAreaField('Pitch comment', validators=[Required()])
     submit = SubmitField('Submit')
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class AddPitchForm(FlaskForm):
+    category = SelectField('category', choices=[('pickup-lines','pickup-lines'),('Interview-pitch','Interview-pitch'), ('Promotion-pitch', 'Promotion-pitch')])
+    content= TextAreaField('Pitch', validators=[Required()])
     submit = SubmitField('Submit')
